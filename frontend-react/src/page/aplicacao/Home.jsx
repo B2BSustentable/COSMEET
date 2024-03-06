bdac16da3abe132816bb6df4588c0ae64e1e07d7
kanaiama <wilson.kanaiamajr@sptech.school>
2024-03-06 15:17:05 -0300

    removendo filtro e reajustando card

--- frontend-react/src/page/aplicacao/Home.jsx
+++ frontend-react/src/page/aplicacao/Home.jsx
@@ -1,37 +1,15 @@
 import styles from "./Home.module.css";
 import "../../styles.css";
 import { useState } from 'react';
-import SearchCategory from "../../component/SearchCategory";
 import CardEmpresa from "../../component/card/Card";
 import HomeLeft from "../../component/homeApp/HomeLeft";
-import nivea from "../../assets/nivea.svg";
-import logo from "../../assets/logo.png";
 
 export default function Home() {
-  const [selectedCategories, setSelectedCategories] = useState([]);
-
-  const handleCategoryChange = (category) => {
-    const index = selectedCategories.findIndex((cat) => cat.id === category.id);
-    if (index === -1) {
-      setSelectedCategories([...selectedCategories, category]);
-    } else {
-      setSelectedCategories(selectedCategories.filter((cat) => cat.id !== category.id));
-    }
-  };
-
   return (
     <>
       <div className={styles.main}>
         <HomeLeft />
         <div className={styles.right}>
-          <div className={styles.filter}>
-            <span>Filtros ativos:</span>
-            {selectedCategories.map((category, index) => (
-              <div key={index} className={styles.categoryBox}>
-                <span>{category.title}</span>
-              </div>
-            ))}
-          </div>
           <div className={styles.columnContainer}>
             {[...Array(30)].map((_, index) => (
               <div key={index} className={styles.cardWrapper}>
