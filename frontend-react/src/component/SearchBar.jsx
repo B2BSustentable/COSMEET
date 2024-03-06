583007193f27f42af091ef926c73381724ba327c
kanaiama <wilson.kanaiamajr@sptech.school>
2024-03-05 21:15:12 -0300

    voltando a search bar

---
+++ frontend-react/src/component/SearchBar.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+
+export default function SearchBar() {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+    </Paper>
+  );
+}
