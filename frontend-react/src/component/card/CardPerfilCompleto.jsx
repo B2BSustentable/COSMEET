c7cc0b6e8e5192c6d427ad0ef2226245617bd09d
kanaiama <wilson.kanaiamajr@sptech.school>
2024-03-15 19:04:45 -0300

    user.js func getEmpresa

--- frontend-react/src/component/card/CardPerfilCompleto.jsx
+++ frontend-react/src/component/card/CardPerfilCompleto.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import "../../styles.css";
 import styles from "./CardPerfilCompleto.module.css";
 import nivea from "../../assets/nivea.svg";
@@ -6,9 +6,24 @@ import close from "../../assets/close.png";
 import unfav from "../../assets/unfav.svg";
 import fav from "../../assets/fav.svg";
 import { Link } from "react-router-dom";
+import { getEmpresa } from "../../../api/services/user"; 
 
 export default function CardPerfilCompleto() {
+  const [empresa, setEmpresa] = useState(null); 
   const [isFavorited, setIsFavorited] = useState(false);
+
+  useEffect(() => {
+    async function fetchEmpresa() {
+      try {
+        const empresaData = await getEmpresa(); 
+        setEmpresa(empresaData); 
+      } catch (error) {
+        console.error("Erro ao buscar dados da empresa:", error);
+      }
+    }
+
+    fetchEmpresa();
+  }, []);
 
   const handleFavoritedToggle = () => {
     setIsFavorited(!isFavorited);
@@ -16,55 +31,54 @@ export default function CardPerfilCompleto() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.topo}>
-        <div className={styles.img_info}>
-          <img src={nivea} alt="" />
-          <div className={styles.info}>
-            <b>Nome</b>
-            <span>Distribuidor</span>
-            <span>Guarulhos, São Paulo, Brazil</span>
+      {empresa ? (
+        <>
+          <div className={styles.topo}>
+            <div className={styles.img_info}>
+              <img src={nivea} alt="" />
+              <div className={styles.info}>
+                <b>{empresa.name}</b>
+                <span>{empresa.occupation}</span>
+                <span>Guarulhos, São Paulo, Brazil</span>
+              </div>
+            </div>
+            <div className={styles.close_unfav}>
+              <Link to="/home">
+                <img src={close} className={styles.img_close} alt="X" />
+              </Link>
+              <img
+                src={isFavorited ? fav : unfav}
+                className={styles.img_close}
+                alt=""
+                onClick={handleFavoritedToggle}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.close_unfav}>
-          <Link to="/home">
-            <img src={close} className={styles.img_close} alt="X" />
-          </Link>
-          <img
-            src={isFavorited ? fav : unfav}
-            className={styles.img_close}
-            alt=""
-            onClick={handleFavoritedToggle}
-          />
-        </div>
-      </div>
 
-      <div className={styles.contato}>
-        <h1>Contatos</h1>
-        <div className={styles.tel}>
-          <span>
-            <b>tel</b>: 11 98715-0608
-          </span>
-        </div>
-        <div className={styles.email}>
-          <span>
-            <b>e-mail</b>: w-jr2003@hotmail.com
-          </span>
-        </div>
-      </div>
+          <div className={styles.contato}>
+            <h1>Contatos</h1>
+            <div className={styles.tel}>
+              <span>
+                <b>tel</b>: {empresa.phone}
+              </span>
+            </div>
+            <div className={styles.email}>
+              <span>
+                <b>e-mail</b>: {empresa.email}
+              </span>
+            </div>
+          </div>
 
-      <div className={styles.description}>
-        <h1>Sobre</h1>
-        <div className={styles.description_company}>
-          <p>
-            A NIVEA celebra seu segundo aniversário, mantendo seu compromisso
-            com a inovação e qualidade na cosmética. Sua missão é promover a
-            beleza autêntica e o bem-estar dos clientes, oferecendo produtos de
-            alta qualidade para cuidar da pele e garantir sua saúde e radiância.
-            A empresa é apaixonada pela ciência da beleza e comprometida com a
-            satisfação do cliente.
-          </p>
-        </div>
-      </div>
+          <div className={styles.description}>
+            <h1>Sobre</h1>
+            <div className={styles.description_company}>
+              <p>{empresa.about}</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Carregando...</p>
+      )}
     </div>
   );
 }
