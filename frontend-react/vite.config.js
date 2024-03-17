628d5255fcb151c76a8ed3fbe0dda9b321dc2cd5
jgmatosmota <jose.mota@sptech.school>
2024-03-17 20:54:58 -0300

    vit

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist', // diretório de saída para os arquivos construídos
+    emptyOutDir: true, // limpa o diretório de saída antes de construir
+    sourcemap: false, // gera sourcemaps para depuração
+  },
+});
