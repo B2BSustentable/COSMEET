fdfb01e41c91c8bf17f9b33e6b2e0633e7343d79
jgmatosmota <jose.mota@sptech.school>
2024-03-17 21:05:45 -0300

    correcao

--- backend-node/server.js
+++ backend-node/server.js
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,23 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  build: {
-    outDir: 'dist', // diretório de saída para os arquivos construídos
-    emptyOutDir: true, // limpa o diretório de saída antes de construir
-    sourcemap: false, // gera sourcemaps para depuração
-  },
-});
+})
